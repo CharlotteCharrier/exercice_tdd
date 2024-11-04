05f9e304f7090470b58bec1259f3ce35bde90982
CHARRIER Charlotte <158159781+CharlotteCharrier@users.noreply.github.com>
2024-11-04 15:45:20 +0100

    initialisation du projet agenda

---
+++ src/main/java/fr/simplon/exercice_tdd/ExerciceTddApplication.java
@@ -0,0 +1,13 @@
+package fr.simplon.exercice_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciceTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/simplon/exercice_tdd/ExerciceTddApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.simplon.exercice_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
